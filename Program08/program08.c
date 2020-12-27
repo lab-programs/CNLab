f10c0e1a1d27486b07349a1909212f1f0d0bedc2
Tejas M R <matblockgreninja@gmail.com>
2020-12-27 21:28:30 +0530

    donezo

--- Program08/program08.c
+++ Program08/program08.c
@@ -2,20 +2,18 @@
 #include<stdlib.h>
 #define MAX 5
 
-int pq[MAX], f=-1, r=-1;
+int pq[MAX+1], f=-1, r=-1;
 
-int isEmpty() {
-    
-}
 
 void enqueue() {
-    if(r >= n-1) {
-        print("Overflow!\n");
+    if(r >= MAX-1) {
+        printf("Overflow!\n");
     }
     else {
         if(f == -1) {
             f = 0;
         }
+        r++;
         int value;
         printf("Enter an element: ");
         scanf("%d", &value);
@@ -24,14 +22,61 @@ void enqueue() {
         // Iterate from f to r-1
         for(int i=f; i<r; i++) {
             // When you find the correct priority
-            if(pq[i] > item) {
-                // 
+            if(pq[i] > value) {
+                //
                 for(int j=r-1; j>= i; j--) {
-                    pq[i+1] = pq[i];
+                    pq[j+1] = pq[j];
                 }
-                pq[i] = item;
+                pq[i] = value;
                 return;
             }
         }
+        pq[r] = value;
     }
+}
+
+void dequeue() {
+    if(f == -1 || r == -1) {
+        r = -1;
+        f = -1;
+        printf("Underflow\n");
+        return;
+    }
+    printf("%d deleted\n", pq[f]);
+    for(int i=f; i<=r; i++) {
+        pq[i] = pq[i+1];
+    }
+    r--;
+}
+
+void display() {
+    if(f != -1 && r != -1) {
+        printf("Priority Queue: ");
+        for(int i=f; i<=r; i++) {
+            printf("%d ", pq[i]);
+        }
+        printf("\n");
+        return;
+    }
+    printf("Underflow\n");
+}
+
+int main() {
+    int ch;
+    printf("1. Enqueue   2. Dequeue   3. Display   4. Exit\n");
+    do {
+        printf("Enter: ");
+        scanf("%d", &ch);
+        switch(ch) {
+            case 1: enqueue();
+                break;
+            case 2: dequeue();
+                break;
+            case 3: display();
+                break;
+            case 4: printf("Terminated...\n");
+                break;
+            default: printf("Invalid choice\n");
+        }
+    } while(ch != 4);
 }
